efee37a9bf606f580c703cc8a92ed4b2d2c02deb
Sunior <speedle71@naver.com>
2026-05-12 19:02:40 +0900

    fix: include runner for all Android library tests

--- build-logic/convention/src/main/java/river.android.library.gradle.kts
+++ build-logic/convention/src/main/java/river.android.library.gradle.kts
@@ -1,7 +1,12 @@
 import com.river.walklog.configureKotlinAndroid
+import com.river.walklog.findLibrary
 
 plugins {
     id("com.android.library")
 }
 
 configureKotlinAndroid()
+
+dependencies {
+    "androidTestImplementation"(findLibrary("androidx-runner").get())
+}
