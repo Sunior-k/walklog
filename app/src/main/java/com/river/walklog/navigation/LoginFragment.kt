195d8f2b48bd12ca3547939ea22b41a85fb598ce
Sunior <speedle71@naver.com>
2026-07-02 17:37:43 +0900

    feat: add feature:login screen with Google sign-in

---
+++ app/src/main/java/com/river/walklog/navigation/LoginFragment.kt
@@ -0,0 +1,38 @@
+package com.river.walklog.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.river.walklog.R
+import com.river.walklog.core.designsystem.foundation.WalkLogTheme
+import com.river.walklog.feature.login.LoginRoute
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View = ComposeView(requireActivity()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        setContent {
+            WalkLogTheme {
+                LoginRoute(
+                    onNavigateToOnboarding = {
+                        findNavController().navigate(R.id.action_login_to_onboarding)
+                    },
+                    onNavigateToHome = {
+                        findNavController().navigate(R.id.action_login_to_home)
+                    },
+                )
+            }
+        }
+    }
+}
